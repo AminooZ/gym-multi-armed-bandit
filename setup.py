9f9f9f27eb2de8ba3141d0f76932731aa434db68
mazghal <aminezghal@vente-privee.com>
2020-05-09 19:06:42 +0200

    remove pointing to non existing src dir

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-from setuptools import find_namespace_packages
 from setuptools import setup
 
 setup(
     name='gym_multi_armed_bandits',
     author='Mohamed Amine ZGHAL',
-    packages=find_namespace_packages('src'),
-    package_dir={'': 'src'},
     python_requires='>=3.6.*',
     version='0.0.1',
     install_requires=[
